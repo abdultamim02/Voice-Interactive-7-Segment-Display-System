89e8fcd11948c3386e798dd180662d3fb4edb599
Abdul Karim Tamim <116204905+abdultamim02@users.noreply.github.com>
2024-10-21 01:30:56 -0700

    Update system_testbench.v

--- Voice Interactive 7-Segment Display System/Simulation Sources/system_testbench.v
+++ Voice Interactive 7-Segment Display System/Simulation Sources/system_testbench.v
@@ -13,20 +13,25 @@ module system_testbench;
     reg clk;
     reg reset;
     reg nRESET;                         // Active Low reset
-    reg [N*4-1:0] address;              // 16 bit input addrerss for ---> ProgramAddressMap and DataMemoryAddress modules <--- 
+    reg [N*8-1:0] address;              // 32 bit input addrerss for ---> ProgramAddressMap and DataMemoryAddress modules <--- 
+    reg [5:0] ID;                       // 6 bit input ID for a specifc number for ---> DigitTo7SegmentDisplay.v module <---
     wire signed [7:0] out;              // Sine wave output from the ---> read_memory.v module <---
     wire [N-1:0] lfsr_4bit;             // 4 bit output from ---> RandomNoiseLFSR.v module <---
     wire [N*2-1:0] lfsr_8bit;           // 8 bit output from ---> RandomNoiseLFSR.v module <---
     wire [N*8-1:0] lfsr_32bit;          // 32 bit output from ---> RandomNoiseLFSR.v module <---
-    wire [N*2-1:0] SRAM_0;              // 8 bit output first SRAM for ---> ProgramAddressMap module <---
-    wire [N*2-1:0] SRAM_1;              // 8 bit output second SRAM for ---> ProgramAddressMap module <---
-    wire [N*2-1:0] Output_Port;         // 8 bit output Output Port for ---> ProgramAddressMap module <---
-    wire [N*2-1:0] Input_Port;          // 8 bit output Input Port for ---> ProgramAddressMap module <---
+    wire [N*4-1:0] SRAM_0;              // 16 bit output first SRAM for ---> ProgramAddressMap module <---
+    wire [N*4-1:0] SRAM_1;              // 16 bit output second SRAM for ---> ProgramAddressMap module <---
+    wire [N*4-1:0] UART1;               // 16 bit output UART1 for ---> ProgramAddressMap module <---
+    wire [N*4-1:0] Control_Module;      // 16 bit output Control Module for ---> ProgramAddressMap module <---
     wire [N/2:0] active_select;         // 2 bit active select (indicating which memory I/O was chosen) for ---> ProgramAddressMap module <---
-    wire [N*2-1:0] Flash_0;             // 8 bit output first Flash for ---> DataMemoryAddress module <---
-    wire [N*2-1:0] Flash_1;             // 8 bit output second Flash for ---> DataMemoryAddress module <---
+    wire [N*4-1:0] Flash_0;             // 16 bit output first Flash for ---> DataMemoryAddress module <---
+    wire [N*4-1:0] Flash_1;             // 16 bit output second Flash for ---> DataMemoryAddress module <---
     wire [1:0] chip_select;             // 1 bit chip select (indicating which memory IC was chosen) for ---> DataMemoryAddress module <---
     wire CE;                            // 1 bit output Chip Enable for ---> SRAM_Chip_Enable module <---
+    wire OE;                            // 1 bit output Output Enable for ---> SRAM_Chip_Enable module <---
+    wire WE;                            // 1 bit output Write Enable for ---> SRAM_Chip_Enable module <---
+    wire WP;                            // 1 bit output Write Protect for ---> SRAM_Chip_Enable module <---
+    wire [13:0] Seven_Segment_Display;  // 14 bit output testing for seven segment display for ---> DigitTo7SegmentDisplay.v module <---
     
     parameter ClockPeriod = 10;     // ClockPeriod is 10 ns
     
@@ -39,6 +44,7 @@ module system_testbench;
          (.clk(clk),
           .reset(reset),
           .nRESET(nRESET),
+          .ID(ID),
           .address(address),
           .out(out),
           .lfsr_4bit(lfsr_4bit),
@@ -46,13 +52,17 @@ module system_testbench;
           .lfsr_32bit(lfsr_32bit),
           .SRAM_0(SRAM_0),
           .SRAM_1(SRAM_1),
-          .Output_Port(Output_Port),
-          .Input_Port(Input_Port),
+          .UART1(UART1),
+          .Control_Module(Control_Module),
           .active_select(active_select),
           .Flash_0(Flash_0),
           .Flash_1(Flash_1),
           .chip_select(chip_select),
-          .CE(CE)
+          .CE(CE),
+          .OE(OE),
+          .WE(WE),
+          .WP(WP),
+          .Seven_Segment_Display(Seven_Segment_Display)
           );
 
     initial begin
@@ -65,25 +75,66 @@ module system_testbench;
         reset = 0;      // Deactivte reset
         nRESET = 1;     // Activte nREST
         
+        // Flash Addresses Testing
         #30;
-        address = 16'h1AB9;      // An address between 0000 - 1FFFF
+        address = 32'h0045_ABCD;      // An address between 0x0000_0000 to 0x07FF_FFFF
         
         #30;
-        address = 16'h33A7;      // An address between 2000 - 3FFFF
+        address = 32'h0901_DCBA;      // An address between 0x0800_0000 to 0x0FFF_FFFF
+        
+        // SRAM Address Testing
+        #60;
+        address = 32'h12AB_78AD;      // An address between 0x1000_0000 to 0x13FF_FFFF
         
         #30;
-        address = 16'h58FB;      // An address between 4000 - 5FFFF
+        address = 32'h1649_EF32;      // An address between 0x1400_0000 to 0x17FF_FFFF
         
         #30;
-        address = 16'h700F;      // An address between 6000 - 7FFFF
+        address = 32'h4802_2FFA;      // An address between 0x4802_2000 to 0x4802_2FFF
         
+        #30;
+        address = 32'h44E1_0ABC;      // An address between 0x44E1_0000 to 0x44E1_1FFF
+        
+        #30;
+        ID = 0;                      // Start with ID = 0 (IDEL state)
+        
+        #30;
+        ID = 5;                      // Change ID to 5 (move to START state)
+        
+        #30;
+        ID = 13;                     // Record number
+        
+        #30;
+        ID = 35;                     // Record number
+        
+        #30;
+        ID = 44;                     // Record number
+        
+        #30;
+        ID = 46;                     // 46 Indicates no more numbers (move to DONE state)
+        
+        #30;
+        ID = 47;                     // 47 indicates there is another number to display (go back to START state)
+        
+        #30;
+        ID = 30;                     // Record number
+        
+        #30;
+        ID = 38;                     // Record number
+        
+        #30;
+        ID = 46;                     // 46 Indicates no more numbers (move to DONE state)
+        
+        #30;
+        ID = 0;                      // 0 Indicates no more numbers (back to IDEL state)
+                
         // Generate noise for a certain number of clock cycles
         // repeat (50) begin
         //    #10; // Wait for 10ns (one clock period)
         // end
         
-        // Simulate for 400ns
-        #400;
+        // Simulate for 100ns
+        #50;
         $finish;
     end
     
