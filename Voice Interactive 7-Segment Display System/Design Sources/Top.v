b22ae152175d035689e0b8961e9aae4f0b80c145
Abdul Karim Tamim <116204905+abdultamim02@users.noreply.github.com>
2024-10-27 23:27:37 -0700

    Update Top.v

--- Voice Interactive 7-Segment Display System/Design Sources/Top.v
+++ Voice Interactive 7-Segment Display System/Design Sources/Top.v
@@ -10,22 +10,23 @@ module Top #(parameter N = 4)   // Default width = 4
              input nRESET,                                  // Active Low reset
              input [5:0] ID,                                // 6 bit input ID for a specifc number for ---> DigitTo7SegmentDisplay.v module <---
              input [N*8-1:0] address,                       // 32 bit input addrerss for ---> ProgramAddressMap and DataMemoryAddress modules <--- 
+             input read,                                    // 1 bit input read signal for ---> DataMemoryAddress module <---
+             input write,                                   // 1 bit input write signal for ---> DataMemoryAddress module <---
              output wire signed [7:0] out,                  // Declare output as signed for ---> read_memory.v module <---
              output wire [N-1:0] lfsr_4bit,                 // Declare lfsr_4bit for ---> RandomNoiseLFSR.v module <---
              output wire [N*2-1:0] lfsr_8bit,               // Declare lfsr_8bit for ---> RandomNoiseLFSR.v module <---
              output wire [N*8-1:0] lfsr_32bit,              // Declare lfsr_32bit for ---> RandomNoiseLFSR.v module <---
-             output wire [N*4-1:0] SRAM_0,                  // 16 bit output first SRAM for ---> ProgramAddressMap module <---
-             output wire [N*4-1:0] SRAM_1,                  // 16 bit output second SRAM for ---> ProgramAddressMap module <---
-             output wire [N*4-1:0] UART1,                   // 16 bit output UART1 for ---> ProgramAddressMap module <---
-             output wire [N*4-1:0] Control_Module,          // 16 bit output Control Module for ---> ProgramAddressMap module <---
-             output wire [2:0] active_select,               // 2 bit active select (indicating which memory I/O was chosen) for ---> ProgramAddressMap module <---
-             output wire [N*4-1:0] Flash_0,                 // 16 bit output first Flash for ---> DataMemoryAddress module <---
-             output wire [N*4-1:0] Flash_1,                 // 16 bit output second Flash for ---> DataMemoryAddress module <---
-             output wire [1:0] chip_select,                 // 1 bit chip select (indicating which memory I/O was chosen) for ---> DataMemoryAddress module <---
-             output wire CE,                                // 1 bit output Chip Enable for ---> SRAM_Chip_Enable module <---
-             output wire OE,                                // 1 bit output Chip Enable for ---> SRAM_Chip_Enable module <---
-             output wire WE,                                // 1 bit output Chip Enable for ---> SRAM_Chip_Enable module <---
-             output wire WP,                                // 1 bit output Chip Enable for ---> SRAM_Chip_Enable module <---
+             output wire [N*4-1:0] Control_Module,          // 16 bit output Control Module for ---> DataMemoryAddress module <---
+             output wire [N*4-1:0] UART1,                   // 16 bit output UART1  for ---> DataMemoryAddress module <---
+             output wire CE0,                               // 1 bit output first SRAM Chip Enable for ---> DataMemoryAddress module <---
+             output wire CE1,                               // 1 bit output second SRAM Chip Enable for ---> DataMemoryAddress module <---
+             output wire OE0,                               // 1 bit output first SRAM Output Enable for ---> DataMemoryAddress module <---
+             output wire OE1,                               // 1 bit output second SRAM Output Enable for ---> DataMemoryAddress module <---
+             output wire WE0,                               // 1 bit output first SRAM Write Enable for ---> DataMemoryAddress module <---
+             output wire WE1,                               // 1 bit output second SRAM Write Enable for ---> DataMemoryAddress module <---
+             output wire CS0,                               // 1 bit output first Flash CS0 for ---> ProgramAddressMap module <---
+             output wire CS1,                               // 1 bit output second Flash CS1 for ---> ProgramAddressMap module <---
+             output wire WP,                                // 1 bit output Chip Enable for ---> ProgramAddressMap module <---
              output wire [N*3+1:0] Seven_Segment_Display    // 14 bit output testing for seven segment display for ---> DigitTo7SegmentDisplay.v module <---
              );
     
@@ -54,31 +55,26 @@ module Top #(parameter N = 4)   // Default width = 4
     ProgramAddressMap #(.N(N*8)) dataAdd (.clk(clk),
                                           .nRESET(nRESET),
                                           .address(address),
-                                          .Flash_0(Flash_0),
-                                          .Flash_1(Flash_1),
-                                          .chip_select(chip_select)
+                                          .CS0(CS0),
+                                          .CS1(CS1),
+                                          .WP(WP)
                                          );
     
     // Instantiation of the DataMemoryAddress module
     DataMemoryAddress #(.N(N*8)) progAdd (.clk(clk),
                                           .nRESET(nRESET),
                                           .address(address),
-                                          .SRAM_0(SRAM_0),
-                                          .SRAM_1(SRAM_1),
-                                          .UART1(UART1),
+                                          .read(read),
+                                          .write(write),
                                           .Control_Module(Control_Module),
-                                          .active_select(active_select)
+                                          .UART1(UART1),
+                                          .CE0(CE0),
+                                          .CE1(CE1),
+                                          .OE0(OE0),
+                                          .OE1(OE1),
+                                          .WE0(WE0),
+                                          .WE1(WE1)
                                          );
-                                         
-    // Instantiation of the SRAM_Chip_Enable module
-    Flash_SRAM_Control_Signals #(.N(N*8)) controlSig (.clk(clk),
-                                                      .nRESET(nRESET),
-                                                      .address(address),
-                                                      .CE(CE),
-                                                      .OE(OE),
-                                                      .WE(WE),
-                                                      .WP(WP)
-                                                      );
     
     // Instantiation of the DigitTo7SegmentDisplay module
     DigitTo7SegmentDisplay display (.clk(clk), 
