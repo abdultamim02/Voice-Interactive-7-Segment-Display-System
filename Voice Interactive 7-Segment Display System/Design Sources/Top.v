3ae31d72d336c4b177a339ecf3616c34e2ec99b3
Abdul Karim Tamim <116204905+abdultamim02@users.noreply.github.com>
2024-11-03 23:11:34 -0800

    Update Top.v

--- Voice Interactive 7-Segment Display System/Design Sources/Top.v
+++ Voice Interactive 7-Segment Display System/Design Sources/Top.v
@@ -8,10 +8,10 @@ module Top #(parameter N = 4)   // Default width = 4
             (input clk,
              input reset,                                   // Avtive High reset
              input nRESET,                                  // Active Low reset
-             input [5:0] ID,                                // 6 bit input ID for a specifc number for ---> DigitTo7SegmentDisplay.v module <---
              input [N*8-1:0] address,                       // 32 bit input addrerss for ---> ProgramAddressMap and DataMemoryAddress modules <--- 
              input read,                                    // 1 bit input read signal for ---> DataMemoryAddress module <---
              input write,                                   // 1 bit input write signal for ---> DataMemoryAddress module <---
+             input rx,                                      // Serial data input bit (1-bit) for ---> UART_Receiver module <---
              output wire signed [7:0] out,                  // Declare output as signed for ---> read_memory.v module <---
              output wire [N-1:0] lfsr_4bit,                 // Declare lfsr_4bit for ---> RandomNoiseLFSR.v module <---
              output wire [N*2-1:0] lfsr_8bit,               // Declare lfsr_8bit for ---> RandomNoiseLFSR.v module <---
@@ -27,6 +27,8 @@ module Top #(parameter N = 4)   // Default width = 4
              output wire CS0,                               // 1 bit output first Flash CS0 for ---> ProgramAddressMap module <---
              output wire CS1,                               // 1 bit output second Flash CS1 for ---> ProgramAddressMap module <---
              output wire WP,                                // 1 bit output Chip Enable for ---> ProgramAddressMap module <---
+             output wire [7:0] data,                        // 8-bit data to transmit for ---> UART_Receiver module <---
+             output wire rx_busy,                           // Signal to indicate whether transmission is -> busy <- or not (tx_busy = 1: transmitter is busy and currently involved in transmitting data | tx_done = 0: transmitter is not busy or has completed its transmission) for ---> UART_Receiver module <---
              output wire [N*3+1:0] Seven_Segment_Display    // 14 bit output testing for seven segment display for ---> DigitTo7SegmentDisplay.v module <---
              );
     
@@ -76,10 +78,19 @@ module Top #(parameter N = 4)   // Default width = 4
                                           .WE1(WE1)
                                          );
     
+    // Instantiation of the UART_Receiver module
+    UART_Receiver Receiver (.clk(clk),
+                            .rx(rx),
+                            .data(data),
+                            .rx_busy(rx_busy)
+                            );
+    
     // Instantiation of the DigitTo7SegmentDisplay module
+    wire [7:0] ID = {data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7]};
+    
     DigitTo7SegmentDisplay display (.clk(clk), 
                                     .reset(reset), 
-                                    .ID(ID),
+                                    .ID(ID[5:0]),
                                     .Seven_Segment_Display(Seven_Segment_Display)
                                     );
     
