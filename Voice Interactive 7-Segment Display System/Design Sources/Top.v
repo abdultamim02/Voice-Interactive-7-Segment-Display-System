21b2041128f758ed344584169fef71c53e4f206f
Abdul Karim Tamim <116204905+abdultamim02@users.noreply.github.com>
2024-11-09 16:58:10 -0800

    Update Top.v

--- Voice Interactive 7-Segment Display System/Design Sources/Top.v
+++ Voice Interactive 7-Segment Display System/Design Sources/Top.v
@@ -8,14 +8,13 @@ module Top #(parameter N = 4)   // Default width = 4
             (input clk,
              input reset,                                   // Avtive High reset
              input nRESET,                                  // Active Low reset
-             input [N*8-1:0] address,                       // 32 bit input addrerss for ---> ProgramAddressMap and DataMemoryAddress modules <--- 
-             input read,                                    // 1 bit input read signal for ---> DataMemoryAddress module <---
-             input write,                                   // 1 bit input write signal for ---> DataMemoryAddress module <---
-             input rx,                                      // Serial data input bit (1-bit) for ---> UART_Receiver module <---
              output wire signed [7:0] out,                  // Declare output as signed for ---> read_memory.v module <---
              output wire [N-1:0] lfsr_4bit,                 // Declare lfsr_4bit for ---> RandomNoiseLFSR.v module <---
              output wire [N*2-1:0] lfsr_8bit,               // Declare lfsr_8bit for ---> RandomNoiseLFSR.v module <---
              output wire [N*8-1:0] lfsr_32bit,              // Declare lfsr_32bit for ---> RandomNoiseLFSR.v module <---
+             input [N*8-1:0] address,                       // 32 bit input addrerss for ---> ProgramAddressMap and DataMemoryAddress modules <--- 
+             input read,                                    // 1 bit input read signal for ---> DataMemoryAddress module <---
+             input write,                                   // 1 bit input write signal for ---> DataMemoryAddress module <---
              output wire [N*4-1:0] Control_Module,          // 16 bit output Control Module for ---> DataMemoryAddress module <---
              output wire [N*4-1:0] UART1,                   // 16 bit output UART1  for ---> DataMemoryAddress module <---
              output wire CE0,                               // 1 bit output first SRAM Chip Enable for ---> DataMemoryAddress module <---
@@ -27,9 +26,12 @@ module Top #(parameter N = 4)   // Default width = 4
              output wire CS0,                               // 1 bit output first Flash CS0 for ---> ProgramAddressMap module <---
              output wire CS1,                               // 1 bit output second Flash CS1 for ---> ProgramAddressMap module <---
              output wire WP,                                // 1 bit output Chip Enable for ---> ProgramAddressMap module <---
-             output wire [7:0] data,                        // 8-bit data to transmit for ---> UART_Receiver module <---
+             input rx,                                      // Serial data input bit (1-bit) for ---> UART_Receiver module <---
              output wire rx_busy,                           // Signal to indicate whether transmission is -> busy <- or not (tx_busy = 1: transmitter is busy and currently involved in transmitting data | tx_done = 0: transmitter is not busy or has completed its transmission) for ---> UART_Receiver module <---
-             output wire [N*3+1:0] Seven_Segment_Display    // 14 bit output testing for seven segment display for ---> DigitTo7SegmentDisplay.v module <---
+             output wire [7:0] data,                        // 8-bit data to transmit for ---> UART_Receiver module <---
+             output wire [N*3+1:0] Seven_Segment_Display,   // 14 bit output testing for seven segment display for ---> DigitTo7SegmentDisplay.v module <---
+             input [31:0] noisy_data,                       // 32-bit noisy input data for  ---> Low_Pass_Filter module <---
+             output wire [31:0] filtered_data                // 32-bit filtered output data for ---> Low_Pass_Filter module <---
              );
     
     // Instantiation of the read_memory module
@@ -93,5 +95,12 @@ module Top #(parameter N = 4)   // Default width = 4
                                     .ID(ID[5:0]),
                                     .Seven_Segment_Display(Seven_Segment_Display)
                                     );
+    
+    // Instantiate the Low_Pass_Filter module
+    Low_Pass_Filter filter (.clk(clk),
+                            .reset(reset),
+                            .noisy_data(noisy_data),
+                            .filtered_data(filtered_data)
+                        );
     
 endmodule
